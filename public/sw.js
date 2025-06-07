01d137ded7b3b5e264e2ceb4e8b0c75125bad743
unknown <Muhammad-Hassan-github>
2025-06-07 16:02:33 +0500

    first commit

---
+++ public/sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "meter-app-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    }).catch((err) => {
+      console.error("Cache addAll failed:", err);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
